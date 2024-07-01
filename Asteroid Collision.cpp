b07f6cedaad33f3f0d7845ea0bb10d057f129cb2
SUSHANTH R HEGDE <120232856+SushanthRHegde@users.noreply.github.com>
2024-07-01 12:48:56 +0530

    Create Asteroid Collision.cpp

---
+++ Asteroid Collision.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    vector<int> asteroidCollision(vector<int>& asteroids) {
+
+        stack<int> st;
+        vector<int> ans;
+        int n = asteroids.size();
+
+        for (int i = 0; i < n; i++) {
+            if (asteroids[i] > 0) {
+                st.push(asteroids[i]);
+            } 
+            else {
+                // 
+                if (!st.empty()  &&
+                       st.top() < abs(asteroids[i])) {
+                    st.pop();
+                }
+                if (!st.empty() && st.top() == abs(asteroids[i])) {
+                    st.pop(); // Both asteroids are destroyed ( equal) --> again pop
+                } else if (st.empty() || st.top() < 0) {
+                    //if first ele is -ve 
+                    st.push(asteroids[i]);
+                }
+            }
+        }
+
+        while (!st.empty()) {
+            ans.push_back(st.top());
+            st.pop();
+        }
+        reverse(ans.begin(), ans.end());
+        return ans;
+    }
+};
